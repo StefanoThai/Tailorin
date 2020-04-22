a37fcf513959ce8e8c8203880653d4baf98f08b5
Ivan G <41164283+IvaninoPanevino@users.noreply.github.com>
2020-04-22 18:20:19 +0200

    Initial commit

---
+++ public_html/README.md
@@ -0,0 +1,11 @@
+                              # Project: Tailorin - WebSite #
+- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+Dal 19/04/20 al 21/04/20
+In questa prima parte del progetto è stato creato il layout principale della Homoepage con eventuali animazioni JS.
+- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+Dal 22/04/20
+Sono state introdotte due pagine:
+    1.fabric.html (layout da ridefinire ma completo)
+    2.Login.html (pieno di bug xd)
+- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+
---
+++ public_html/src/js/main.js
@@ -0,0 +1,65 @@
+/*let nCount = selector => {
+  $(selector).each(function () {
+    $(this)
+      .animate({
+        Counter: $(this).text()
+      }, {
+        // Questa stringa determina per quanto tempo durerà l'animazione.
+        duration: 4000,
+        //Indica quale funzione di andamento utilizzare per la transizione.
+        easing: "swing",
+        /**
+         * A function to be called for each animated property of each animated element. 
+         * This function provides an opportunity to
+         *  modify the Tween object to change the value of the property before it is set.
+         */
+     /*   step: function (value) {
+          $(this).text(Math.ceil(value));
+        }
+      });
+  });
+};
+
+let a = 0;
+$(window).scroll(function () {
+  // Il metodo .offset() ci consente di recuperare la posizione corrente di un elemento rispetto al documento
+  let oTop = $(".numbers").offset().top - window.innerHeight;
+  if (a == 0 && $(window).scrollTop() >= oTop) {
+    a++;
+    nCount(".rect > h1");
+  }
+});*/
+
+
+
+/**
+ *
+ *  sticky navigation
+ *
+ */
+
+let navbar = $(".navbar");
+
+$(window).scroll(function () {
+  // get the complete hight of window
+  let oTop = $(".section-1").offset().top - window.innerHeight;
+  if ($(window).scrollTop() > oTop) {
+    navbar.addClass("sticky");
+  } else {
+    navbar.removeClass("sticky");
+  }
+});
+
+/*
+ * 
+ *  ANIMAZIONE DELL'ANCORAGGIO
+ * 
+ * 
+ */
+$('a[href^="#"]').click(function () {
+    $('html, body').animate({
+        scrollTop: $('[name="' + $.attr(this, 'href').substr(1) + '"]').offset().top
+    }, 500);
+
+    return false;
+});
